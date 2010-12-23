b38ef0c553bb3de95fb5bc77f46fa45a146fc3b9
rech.christian@gmail.com <rech.christian@gmail.com@69070be1-4998-a463-11c1-89231ed85cb9>
2010-12-23 00:19:22 +0000

    Login beta

---
+++ trunk/FireFigtherSheduler/Login/intern.php
@@ -0,0 +1,16 @@
+<?php
+include ("checkuser.php");
+?>
+<html>
+    <head>
+        <title>Interne Seite</title>
+    </head>
+    <body>
+        BenutzerId: <?php echo $_SESSION["user_id"]; ?><br>
+        Nickname: <?php echo $_SESSION["user_email"]; ?><br>
+        Nachname: <?php echo $_SESSION["user_nachname"]; ?><br>
+        Vorname: <?php echo $_SESSION["user_vorname"]; ?>
+        <hr>
+        <a href="logout.php">Ausloggen</a>
+    </body>
+</html>
