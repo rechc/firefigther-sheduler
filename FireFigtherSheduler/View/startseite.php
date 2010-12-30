94a3855a0efed12da3cc6cc2ab291e83cc3ec760
rech.christian <rech.christian@69070be1-4998-a463-11c1-89231ed85cb9>
2010-12-30 11:25:15 +0000

    startseiten test

--- FireFigtherSheduler/View/startseite.php
+++ FireFigtherSheduler/View/startseite.php
@@ -1,12 +1,62 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<?php
+include ("./Model/Includes/checkuser.php");
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>FireFighterScheduler</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="maincontainer">
+            <div id="header" >
+                <div id="branding" class="left"  style="cursor: pointer;" />
+            </div>
+            <div id="buttonbar">
+                <ul>
+                    <li><a href="startseite.php?section=overview">Übersicht</a></li>
+                    <li><a href="#">Komplett Übersicht</a></li>
+                    <li><a href="#">Benutzer</a></li>
+                    <li><a href="#">Statistik</a></li>
+                    <li><a href="startseite.php?section=logout">Logout</a></li>
+                </ul>
+            </div>
+            <div id="content">
+                <?php
+                $section = array();
+                $section['overview'] = 'overview.php';
+                $section['link_2'] = 'overview_all.php';
+                $section['link_3'] = 'users.php';
+                $section['link_4'] = 'statistic.php';
+                $section['logout'] = 'logout.php';
+
+                if (isset($_GET['section'])) {
+                    switch ($_GET['section']) {
+                        case 'link_1': include('link_1.php');
+                            break;
+
+                        case 'link_2': include('link_2.php');
+                            break;
+
+                        case 'link_3': include('link_3.php');
+                            break;
+
+                        case 'logout': header("Location:../Controller/Login/logout.php");;
+                            break;
+                    }
+                } else {
+                    include('login_formular.php');
+                }
+                ?>
+            </div>
+
+        </div>
+        <div id="foot">
+           <a href="http://www.ff-riegelsberg.de/?page_id=30">Impressum</a>
+        </div>
+
     </body>
 </html>
