c738ecba8200b5e0434330946bf6d726a3d1bdce
rech.christian@gmail.com <rech.christian@gmail.com@69070be1-4998-a463-11c1-89231ed85cb9>
2011-01-04 13:53:31 +0000

    An Login gearbeitet (was probiert und doch verworfen)

--- FireFigtherSheduler/Model/Includes/dbConnector.php
+++ FireFigtherSheduler/Model/Includes/dbConnector.php
@@ -1,9 +1,15 @@
 <?php
-    // Zugangsdaten Datenbank
-    $mysqlhost="feuerwehr-saar.de"; // MySQL-Host angeben
-                $mysqldb="db1057229-2"; // Gewuenschte Datenbank angeben
-                $mysqluser="dbu1057229"; // MySQL-User angeben
-                $mysqlpwd="h0m3b0y"; // Passwort angeben
+//    // Zugangsdaten Datenbank
+//    $mysqlhost="feuerwehr-saar.de"; // MySQL-Host angeben
+//    $mysqldb="db1057229-2"; // Gewuenschte Datenbank angeben
+//    $mysqluser="dbu1057229"; // MySQL-User angeben
+//    $mysqlpwd="h0m3b0y"; // Passwort angeben
+
+    //vorübergehenede Zugangsdaten
+    $mysqlhost="stud-i-pr2.htw-saarland.de"; // MySQL-Host angeben
+    $mysqldb="FFS"; // Gewuenschte Datenbank angeben
+    $mysqluser="htwmaps"; // MySQL-User angeben
+    $mysqlpwd="g00gl3m4p5k1ll4"; // Passwort angeben
 
     // Datenbankverbindung aufbauen
     $connectionid = mysql_connect($mysqlhost, $mysqluser, $mysqlpwd)
