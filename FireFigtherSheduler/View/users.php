7f7a50bb09e17d227fafce0fe3ada76256e01176
rech.christian@gmail.com <rech.christian@gmail.com@69070be1-4998-a463-11c1-89231ed85cb9>
2011-01-19 11:33:58 +0000

    ajax php script entworfen

--- FireFigtherSheduler/View/users.php
+++ FireFigtherSheduler/View/users.php
@@ -1,13 +1,27 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>FFS Benutzer</title>
         <link rel="stylesheet" type="text/css" href="css/users.css" />
+        	<script type="text/javascript" src="../Controller/jsXMLHttpRequestHandle.js"></script>
+                <script type="text/javascript">
+                        function sendUserInfoRequest() {
+                          var xmlHttp = getXMLHttp();
+
+                          xmlHttp.onreadystatechange = function() {
+                            if(xmlHttp.readyState == 4) {
+                              HandleResponse(xmlHttp.responseText);
+                            }
+                          }
+                          xmlHttp.open("GET", "../Controller/getUserInfo.php?" + Math.random(), true);
+                          xmlHttp.send(null);
+                        }
+
+                        function HandleResponse(response){
+                          document.getElementById('ResponseDiv').innerHTML = response;
+                        }
+		</script>
     </head>
     <body>
         <h1>Benutzer</h1>
@@ -21,34 +35,39 @@ and open the template in the editor.
         
                 while ($adr = mysql_fetch_array($adressen_query)){
                     echo "<hr>";
-                    echo $adr[email] . "; " . $adr[name] . "<br>";
+                    echo "<a href='startseite.php?userselect=" . $adr[email] ."'>" .
+                            $adr[email] . "; " . $adr[name] . "<br>".
+                        "</a>";
                     echo "<hr>";
                 }
             ?>
         </div>
         <div id="userdata">
-            <table border="0">
-                <tr>
-                    <td><div>Name: <input type ="text"></div></td>
-                    <td><div>Vorname: <input type ="text"></div></td>
-                </tr>
-                <tr>
-                    <td><div>E-Mail: <input type ="text"></div></td>
-                    <td><div>Geburtsdatum: <input type ="text"></div></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <div>Geschlecht:
-                              männlisch <input type="radio" value="maennlisch" name="Geschlecht">
-                              weiblisch <input type="radio" value="weiblisch" name="Geschlecht">
-                         </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td><div><input type="button" value="Ok" name="ok"></div></td>
-                    <td><div><input type ="button" value="abbrechen" name="reset"</div></td>
-                </tr>
-            </table>
+            <form id="editUser" onsubmit="return false;">
+                <table border="0">
+                    <tr>
+                        <td><div>Name: <input type ="text"></div></td>
+                        <td><div>Vorname: <input type ="text"></div></td>
+                    </tr>
+                    <tr>
+                        <td><div>E-Mail: <input type ="text"></div></td>
+                        <td><div>Geburtsdatum: <input type ="text"></div></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div>Geschlecht:
+                                  männlisch <input type="radio" value="maennlisch" name="Geschlecht">
+                                  weiblisch <input type="radio" value="weiblisch" name="Geschlecht">
+                             </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><div><input type="button" value="Ok" name="ok" onClick="sendUserInfoRequest();"></div></td>
+                        <td><div><input type ="button" value="abbrechen" name="reset"</div></td>
+                    </tr>
+                </table>
+            </form>
+                <div id='ResponseDiv'></div>
         </div>
         <?php
         // put your code here
