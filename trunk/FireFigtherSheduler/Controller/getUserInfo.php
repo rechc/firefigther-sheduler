86a0d81099e2e58750b05990426183d17debfc35
rech.christian <rech.christian@69070be1-4998-a463-11c1-89231ed85cb9>
2011-02-04 16:41:27 +0000

    dies und das....

--- trunk/FireFigtherSheduler/Controller/getUserInfo.php
+++ trunk/FireFigtherSheduler/Controller/getUserInfo.php
@@ -8,14 +8,18 @@
 
 //define('ABSPATH', dirname(__FILE__).'\\');
 require_once 'XML.php';
-//require_once '../Model/User.php';
+require_once '../Model/User.php';
 
-$ID=$_GET["userID"];
+$ID= $_GET["userID"];
 
 if($ID != ""){
-//    $user = User::get_user($ID);
-     XML::getXML($ID,"Luky", "Luke", "email", "01.01.1901");
-//    XML::getXML($user->getID(),$user->getVorname(),$user->getName(),$user->getEmail(),$user->getGebDat());
+    $user2 = User::get_user($ID);
+    $user = new User();
+    $user->setName("Name");
+    $user->setVorname("Vorname");
+    $user->setEmail("asd@asd.de");
+    $user->setGebDat("01.01.1900");
+    XML::getXML($ID,$user->getName(), $user->getVorname(),$user->getEmail(),$user->getGebDat());
 }
 
 
---
+++ trunk/FireFigtherSheduler/Trash/jsResetUserManager.js
@@ -0,0 +1,7 @@
+function reset(){
+         document.getElementById('ok').value = "Hinzufügen";
+         document.getElementById('reset').value = "Abbrechen";
+         document.getElementById("delete").style.visibility = "hidden";
+
+         
+}
